4025b47cd97a7c30866e30a851127ef51ab6f04f
UjasBhatt10 <ujasbhatt3531@gmail.com>
2024-02-21 22:19:28 +0530

    portfolio#2

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,93 @@ class Home extends StatelessWidget {
             Row(
               children: <Widget>[
                 CircleAvatar(
-                  radius: 40,
+                  radius: 50,
                 ),
+                SizedBox(width: 50.0),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Name"),
-                    Text("Designation"),
+                    Text(
+                      "Ujas Bhatt",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text("I.T. Engineer", style: TextStyle(fontSize: 18)),
                   ],
                 )
               ],
-            )
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 40.0,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text("Collage Name"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.computer_rounded,
+                        size: 40.0,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text("Project Details"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.location_pin,
+                        size: 40.0,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text("Value"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.mail,
+                        size: 40.0,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text("Value"),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        size: 40.0,
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text("Value"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text("About Me"),
+            Text("Created By Ujas Bhatt"),
           ],
         ),
       ),
